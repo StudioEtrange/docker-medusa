ab2effe2b4072ed13a8d0991db892fca74fcaa58
StudioEtrange <sboucault@gmail.com>
2018-11-20 01:35:10 +0100

    review

--- README.md
+++ README.md
@@ -8,11 +8,11 @@
 * By default medusa configuration files will be in /data/medusa _(You should map a docker volume to /data)_
 
 
-## Sample Usage
+## Quick Usage
 
 for running latest stable version of medusa :
 
-	docker run -d -v $(pwd):/data -p 8081:8081 studioetrange/docker-medusa:latest
+	docker run -d -v $(pwd):/data -v $(pwd)/tv:/tv -p 8081:8081 studioetrange/docker-medusa
 
 then go to http://localhost:8081
 
@@ -20,14 +20,14 @@ then go to http://localhost:8081
 
 Available tag for studioetrange/docker-medusa:__TAG__
 
-	latest, v0.2.10, v0.2.9, v0.2.8, v0.2.7, v0.2.6, v0.2.5, v0.2.4, v0.2.3, v0.2.2, v0.2.1, v0.2.0, v0.1.24, v0.1.23, v0.1.22, v0.1.21, v0.1.20, v0.1.19, v0.1.18, v0.1.17, v0.1.16, v0.1.15, v0.1.14, v0.1.13, v0.1.12, v0.1.12rc1, v0.1.11, v0.1.10, v0.1.9, v0.1.8.1, v0.1.8, v0.1.7.1, v0.1.7, v0.1.6, v0.1.5.1, v0.1.5, v0.1.4.1, v0.1.4, v0.1.3, v0.1.2, v0.1.2rc1, v0.1.1, v0.1.1rc1, v0.1.0, v0.1.0rc2
+	latest, v0.2.12, v0.2.11, v0.2.10, v0.2.9, v0.2.8, v0.2.7, v0.2.6, v0.2.5, v0.2.4, v0.2.3, v0.2.2, v0.2.1, v0.2.0, v0.1.24, v0.1.23, v0.1.22, v0.1.21, v0.1.20, v0.1.19, v0.1.18, v0.1.17, v0.1.16, v0.1.15, v0.1.14, v0.1.13, v0.1.12, v0.1.12rc1, v0.1.11, v0.1.10, v0.1.9, v0.1.8.1, v0.1.8, v0.1.7.1, v0.1.7, v0.1.6, v0.1.5.1, v0.1.5, v0.1.4.1, v0.1.4, v0.1.3, v0.1.2, v0.1.2rc1, v0.1.1, v0.1.1rc1, v0.1.0, v0.1.0rc2
 
-Current latest tag is version __v0.2.10__
+Current latest tag is version __v0.2.12__
 
 ## Instruction
 
 ### build from github source
-	
+
 	git pull https://github.com/StudioEtrange/docker-medusa
 	cd docker-medusa
 	docker build -t=studioetrange/docker-medusa .
@@ -36,18 +36,18 @@ Current latest tag is version __v0.2.10__
 
 	docker pull studioetrange/docker-medusa
 
-### standard usage 
+### standard usage
 
-	docker run -d -v $(pwd):/data -v $(pwd)/tv:/tv -v -p 8081:8081 -e SERVICE_USER=$(id -u):$(id -g) -v /etc/timezone:/etc/timezone:ro -v /etc/localtime:/etc/localtime:ro studioetrange/docker-medusa
-	
+	docker run -d -v $(pwd):/data -v $(pwd)/tv:/tv -p 8081:8081 -e SERVICE_USER=$(id -u):$(id -g) -v /etc/timezone:/etc/timezone:ro -v /etc/localtime:/etc/localtime:ro studioetrange/docker-medusa
+
 ### full run parameters
 
 	docker run -d -v <local path>:/data -v <tv show path>:/tv -p <medusa http port>:8081 -e SERVICE_USER=<uid[:gid]>  -p <supervisor manager http port>:9999 studioetrange/docker-medusa:<version>
 
 	`/data/medusa` will contain medusa configuration and database
 	`/tv` is the root folder of your tv shows
-	
-### run a shell inside this container (without medusa running)
+
+### test a shell inside a new container without medusa running
 
 	docker run -i -t studioetrange/docker-medusa bash
 
--- ver/v0.1.4/docker-entrypoint.sh
+++ ver/v0.1.4/docker-entrypoint.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
 
-# form arg1 : <uid:[gid]>
-# form arg2 : name
+# arg1 (mandatory) : <uid:[gid]> => wanted user/group ids
+# arg2 (mandatory) : name => wanted user name
 # return : user_uid:group_gid:user_name:group_name
 setup_service_user() {
   wanted_user="$1"
   default_name="$2"
-  
+
   user_uid=
   group_gid=
-  
+
   [ -n "$wanted_user" ] && echo "$wanted_user" | grep -q ':' && {
       group_gid="$(echo $wanted_user | cut -d: -f2)"
       user_uid="$(echo $wanted_user | cut -d: -f1)"
   } || user_uid=$wanted_user
-  
+
 
   [[ $user_uid =~ ^[0-9]+$ ]] || return
   if [ -n "$group_gid" ]; then
      [[ $group_gid =~ ^[0-9]+$ ]] || return
   fi
-  
-  
+
+
   # default group name already in used
   if [ -z "$(getent group ${default_name} | cut -d: -f1)" ]; then
     default_group_name=${default_name}
@@ -30,8 +30,8 @@ setup_service_user() {
     # NOTE : we add user_uid instead of group_gid because group_gid can be empty
     default_group_name="${default_name}_${user_uid}"
   fi
- 
-  
+
+
   # we did sepify a group
   if [ -n "$group_gid" ]; then
     original_group_name=$(getent group $group_gid | cut -d: -f1)
@@ -66,7 +66,7 @@ setup_service_user() {
       type useradd &>/dev/null && \
         useradd -M -d /home/${user_name} -s /bin/bash -U -u $user_uid ${user_name} || \
         adduser -H -h /home/${user_name} -D -s /bin/bash -u $user_uid ${user_name}
-        
+
         group_gid=$(getent passwd $user_uid | cut -d: -f4)
         group_name=$(getent group $group_gid | cut -d: -f1)
     else
@@ -80,11 +80,11 @@ setup_service_user() {
     group_gid=$(getent passwd $user_name | cut -d: -f4)
     group_name=$(getent group $group_gid | cut -d: -f1)
   fi
- 
-  
+
+
   mkdir -p /home/${user_name}
   chown -R ${user_uid}:${group_gid} /home/${user_name}
-  
+
   echo ${user_uid}:${group_gid}:${user_name}:${group_name}
 }
 
@@ -94,24 +94,31 @@ setup_service_user() {
 if [ "$1" = "supervisord" ]; then
 
   . activate ${CONDA_ENV}
-  
+
   user_info="$(setup_service_user ${SERVICE_USER} ${SERVICE_NAME})"
 
   SERVICE_USER_UID="$(echo $user_info | cut -d: -f1)"
   SERVICE_GROUP_GID="$(echo $user_info | cut -d: -f2)"
   SERVICE_USER_NAME="$(echo $user_info | cut -d: -f3)"
   SERVICE_GROUP_NAME="$(echo $user_info | cut -d: -f4)"
-  
+
   # export env var to make them available in supervisor context
   export PATH="${PATH}"
   # supervisor user
   export SERVICE_USER_NAME="${SERVICE_USER_NAME}"
   # service name
   export SERVICE_NAME="${SERVICE_NAME}"
-  
-  
-  su "${SERVICE_USER_NAME}" -c "mkdir -p ${SERVICE_DATA_PATH}"
-  
+  # export specific arg for service
+  for a in ${SERVICE_VOLUME_PATH}; do
+    export ${a}=${!a}
+  done
+
+  # volume path
+  # if not exist, create path using service user name
+  for p in ${SERVICE_VOLUME_PATH}; do
+    su "${SERVICE_USER_NAME}" -c "mkdir -p ${p}"
+  done
+
   echo "** Run service as user: $SERVICE_USER_NAME ($SERVICE_USER_UID) group: $SERVICE_GROUP_NAME ($SERVICE_GROUP_GID) **"
 fi
 
