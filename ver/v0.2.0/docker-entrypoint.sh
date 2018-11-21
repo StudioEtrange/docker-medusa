f28fd48d740a4d5c63a61adf56a0168f8bf0cd0c
StudioEtrange <sboucault@gmail.com>
2018-11-22 00:05:42 +0100

    update

--- ver/v0.2.0/docker-entrypoint.sh
+++ ver/v0.2.0/docker-entrypoint.sh
@@ -109,8 +109,8 @@ if [ "$1" = "supervisord" ]; then
   # service name
   export SERVICE_NAME="${SERVICE_NAME}"
   # export specific arg for service
-  for a in ${SERVICE_VOLUME_PATH}; do
-    export ${a}=${!a}
+  for a in ${SERVICE_EXPORT_ARG}; do
+    export ${a}="${!a}"
   done
 
   # volume path
